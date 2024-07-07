4b078f5612ef45c47a86f916539bbb04aa91710e
Hungnt181 <ngothehung02@gmail.com>
2024-07-07 13:32:58 +0700

    Done CRUD-Restore category

--- admin/model/categoryQuery.php
+++ admin/model/categoryQuery.php
@@ -30,6 +30,71 @@ class CategoryQuery {
 
     }
 
+    public function create(Category $category) {
+        try {
+            $sql = "INSERT INTO `category`(`cate_id`, `cate_name`, `cate_status`) VALUES (NULL,'$category->cate_name','$category->cate_status')";
+            $data = $this -> pdo -> exec($sql);
+           
+            if ($data==1) {
+               return "ok";
+            } else {
+                return $data;
+            }
+        } catch (Exception $e) {
+            echo "Lỗi: ".$e ->getMessage();
+            echo "<hr>";
+        }
+    }
+
+
+    public function show_one_cate($cate_id) {
+        try {
+            $sql = "select * from category where cate_id = $cate_id";
+            $data = $this->pdo->query($sql)->fetch();
+           
+            $danhSach = convertToObjectCategory($data);
+            return $data;
+            
+        } catch (Exception $e) {
+            echo "Lỗi: ".$e ->getMessage();
+            echo "<hr>";
+        }
+
+    }
+
+    public function updateCate(Category $category) {
+        try {
+            $sql = "UPDATE `category` SET `cate_name`='$category->cate_name',`cate_status`='$category->cate_status' WHERE `cate_id`='$category->cate_id' ";
+            $data = $this->pdo->exec($sql);
+            if ($data == 1 || $data == 0) {
+                return "ok";
+            } else {
+                return $data;
+            }
+
+        } catch (Exception $e) {
+            echo "Lỗi: ".$e ->getMessage();
+            echo "<hr>";
+        }
+    }
+        
+    public function updateCate_status(Category $category) {
+        try {
+            $sql = "UPDATE `category` SET `cate_status`='$category->cate_status' WHERE `cate_id`='$category->cate_id' ";
+            $data = $this->pdo->exec($sql);
+            if ($data == 1 || $data == 0) {
+                return "ok";
+            } else {
+                return $data;
+            }
+
+        } catch (Exception $e) {
+            echo "Lỗi: ".$e ->getMessage();
+            echo "<hr>";
+        }
+    }
+
+
 
 
 
--- admin/view/category/update.php
+++ admin/view/category/update.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thêm danh mục</title>
+    <title>Cập nhật danh mục</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/be9ed8669f.js" crossorigin="anonymous"></script>
@@ -24,27 +24,28 @@
         <div class="shadow bg-light pb-5 mt-4 ms-4 col-md-8">
              <form action="" class="pb-5 mt-4 ms-4 me-4" method="post">
                     <div>
-                        <h4 class="p-3">Thêm danh mục sản phẩm</h4>
+                        <h4 class="p-3">Cập nhật danh mục sản phẩm</h4>
                     </div>
                     <hr>
+
                     <div class="row">
-                       
+                    <input type="hidden" class="form-control rounded-0" id="inputEmail4" name="cate_id" value="<?=$cate_one['cate_id']?>">
                         <div class="">
                             <label for="inputEmail4" class="form-label">Tên danh mục</label>
-                            <input type="text" class="form-control rounded-0" id="inputEmail4" placeholder="Nhập tên sản phẩm" name="cate_name">
+                            <input type="text" class="form-control rounded-0" id="inputEmail4" placeholder="Nhập tên sản phẩm" name="cate_name" value="<?=$cate_one['cate_name']?>">
                         </div>
                        
                         <div class="mt-3">
                             <span class="form-label">Lựa chọn</span>
                             <div class="row ps-3 pt-2">
                                 <div class="form-check col-2">
-                                    <input class="form-check-input" type="radio" checked value="1" name="cate_status">
+                                    <input class="form-check-input" type="radio"  value="1" name="cate_status" <?= $cate_one['cate_status'] =="1" ? "checked" : "" ?> >
                                     <label class="form-check-label" for="flexRadioDefault1">
                                       Not deleted
                                     </label>
                                 </div>
                                 <div class="form-check col-5">
-                                    <input class="form-check-input" type="radio" value="2" name="cate_status">
+                                    <input class="form-check-input" type="radio" value="0" name="cate_status" <?= $cate_one['cate_status'] !="1" ? "checked" : "" ?>>
                                     <label class="form-check-label" for="flexRadioDefault2">
                                         Deleted
                                     </label>
@@ -53,7 +54,7 @@
                         </div>
 
                         <div class="mt-3 d-flex justify-content-center">
-                            <button type="submit" class="btn btn-success" name='submitFormCreateCate'>Tạo mới</button>
+                            <button type="submit" class="btn btn-success" name='submitFormUpdateCate'>Cập nhật</button>
                         </div>  
                     </div>
                 </form>
